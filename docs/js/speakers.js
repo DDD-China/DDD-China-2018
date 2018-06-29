cff97b7d9036e5750a31c1e562826e84aa71d574
wangyi <yibwang@thoughtworks.com>
2018-06-29 10:33:42 +0800

    add speaker page.

--- docs/js/speakers.js
+++ docs/js/speakers.js
@@ -1,8 +1,11 @@
 $(function () {
-    var id = Number(window.location.search.split('=')[1]) - 1
-    console.log(id)
-    $('.speaker-info .speaker-img img').attr('src', speakers[id]['speaker-img']).attr('alt', speakers[id]['speaker-name']);
-    $('.speaker-info .speaker-detail .speaker-detail-introduction .content').append(speakers[id]['speaker-information']);
-    $('.speaker-info .speaker-detail .speaker-detail-topic .content').append(speakers[id]['speaker-topic']);
-    $('.speaker-info .speaker-detail .speaker-detail-outline .content').append(speakers[id]['speaker-outline']);
+  var id = Number(window.location.search.split('=')[1]) - 1
+  console.log(id)
+  $('.speaker-info .speaker-img img').attr('src', speakers[id]['producer-img']).attr('alt', speakers[id]['producer-name']);
+  $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-name').append(speakers[id]['producer-name']);
+  $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-title').append(speakers[id]['producer-introduction']);
+  $('.speaker-detail .speaker-detail-information').append(speakers[id]['producer-information']);
+
+  $('.speaker-info .speaker-topic .speaker-topic-content').append(speakers[id]['producer-topic']);
+  $('.speaker-info .speaker-topic-detail .speaker-topic-content-detail').append(speakers[id]['topic-information']);
 })
