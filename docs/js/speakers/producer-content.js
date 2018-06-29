1b6d454e41d5c19d967141b4c15b7cddb0e66250
wangyi <yibwang@thoughtworks.com>
2018-06-29 21:49:52 +0800

    change information.

--- docs/js/speakers/producer-content.js
+++ docs/js/speakers/producer-content.js
@@ -8,7 +8,7 @@ var producers = [
     'topic-information': '作为IT系统治理和架构治理的一种工具，企业级架构被很多大型企业广泛采用。但在实际工作当中，企业级架构的定义和治理如何能够和快速变化的业务诉求相匹配，与快速创新的企业愿景相一致，这些问题一直有待解决。DDD作为一种面向响应力的软件和系统设计原则，在企业架构实践中大有用武之地。本专题将专注讨论领域驱动的企业级架构的设计方法和实践。',
     'speakers': [
       {
-        'img': 'resource/face.jpeg',
+        'img': 'resource/liuguangcong.jpeg',
         'name': '刘光聪',
         'introduction': '中兴通讯技术规划部资深系统架构师',
         'information': '敏捷技术教练；就职于中兴通讯，任职资深系统架构师，致力于机器学习算法理论的研究与应用实践；Linux基金会Acumos AI项目TSC（技术控制委员会）成员；热衷于敏捷软件开发，极限编程实践。',
@@ -39,7 +39,7 @@ var producers = [
     'topic-information': '领域驱动设计是建立在领域统一语言（Ubiquitous Language）的基础上的。如何能在业务、开发、运维等不同角色间，构建统一语言，并围绕容易语言构建模型，是很多DDD的实践者以及团队所关注的问题。在本专题中，我们将关注统一语言的发现和使用的过程，以及其为团队所带来的收益。',
     'speakers': [
       {
-        'img': 'resource/face.jpeg',
+        'img': 'resource/wangpeng.jpeg',
         'name': '王鹏',
         'introduction': '华为公司培训服务部，云计算培训方案开发与交付负责人',
         'information': '13年IT行业经验，10年加入华为公司，曾参与华为云存储核心模块设计与开发。当前在运营商培训服务部负责云计算及DevOps相关培训方案开发与交付。本人对面向对象编程思想及DDD有着深刻的理解，并在多个项目中有应用经验。',
@@ -57,7 +57,7 @@ var producers = [
     'topic-information': '随着领域驱动设计实践的展开，团队的技术带头人（Tech Lead）或者架构师（Architect）的工作模式也在发生着变化。架构师的职责从原来的为团队定义架构要素，逐步向为团队提供快速反馈演进。在这一专题中，我们会邀请国内技术团队的一线架构师和技术带头人，通过他们的实际经验的分享，为听众阐明在实践DDD的团队中，架构师的工作到底是什么样的。',
     'speakers': [
       {
-        'img': 'resource/face.jpeg',
+        'img': 'resource/wanghaiwen.jpeg',
         'name': '王海文',
         'introduction': '用友网络科技股份有限公司 高级技术架构师',
         'information': '长期从事大型企业管理软件的领域建模，架构设计。专注于ERP中供应链领域以及智能制造领域的软件研发，涉及ERP，智能工厂，物联网以及云架构方面的研究和实践。对领域驱动设计理论有深入的研究，并在大型toB软件系统中使用DDD的系统分析方法进行落地实践。在运用DDD对软件研发过程优化，传统企业管理软件微服务化，微服务设计等方面有着丰富的架构设计经验。',
@@ -75,7 +75,7 @@ var producers = [
     'topic-information': '2017年，Neal Ford、Rebecca Parson、Patric Kua合著的新书《构建演进式架构》一书问世。一种新的架构治理模式被介绍给业界。演进式架构中对于incremental、guided、multi-dimension三个维度的阐述定义了一种全新的构建架构纬度快速反馈的管理方法。在本专题中，我们会专注演进式架构中三个维度的具体业界实践案例，希望让听众对演进式架构能有进一步的认知。',
     'speakers': [
       {
-        'img': 'resource/face.jpeg',
+        'img': 'resource/zhouyugang.jpeg',
         'name': '周宇刚',
         'introduction': 'ThoughtWorks高级咨询师',
         'information': 'ThoughtWorks高级咨询师，DDD实践家 10年企业级应用开发经验，主要负责软件交付和持续交付咨询。从09年开始关注领域驱动设计，是Stack Overflow DDD标签 All Time Top Answerer 之一，曾在多个行业领域中实战过DDD。',
