92e1ca14042f511f831ee128548bc6cade1f77cc
wangyi <yibwang@thoughtworks.com>
2018-06-29 17:38:56 +0800

    add speakers part under producers introduction.

--- docs/js/speakers.js
+++ docs/js/speakers.js
@@ -1,11 +1,20 @@
 $(function () {
   var id = Number(window.location.search.split('=')[1]) - 1
   console.log(id)
-  $('.speaker-info .speaker-img img').attr('src', speakers[id]['producer-img']).attr('alt', speakers[id]['producer-name']);
-  $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-name').append(speakers[id]['producer-name']);
-  $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-title').append(speakers[id]['producer-introduction']);
-  $('.speaker-detail .speaker-detail-information').append(speakers[id]['producer-information']);
+  console.log(speakers[id])
+  $('.topic-item-1 .speaker-info .speaker-img img').attr('src', speakers[id][0]['producer-img']).attr('alt', speakers[id][0]['producer-name']);
+  $('.topic-item-1 .speaker-info .speaker-detail .speaker-detail-introduction .speaker-name').append(speakers[id][0]['producer-name']);
+  $('.topic-item-1 .speaker-info .speaker-detail .speaker-detail-introduction .speaker-title').append(speakers[id][0]['producer-introduction']);
+  $('.topic-item-1 .speaker-detail .speaker-detail-information').append(speakers[id][0]['producer-information']);
 
-  $('.speaker-info .speaker-topic .speaker-topic-content').append(speakers[id]['producer-topic']);
-  $('.speaker-info .speaker-topic-detail .speaker-topic-content-detail').append(speakers[id]['topic-information']);
-})
+  $('.topic-item-1 .speaker-info .speaker-topic .speaker-topic-content').append(speakers[id][0]['producer-topic']);
+  $('.topic-item-1 .speaker-info .speaker-topic-detail .speaker-topic-content-detail').append(speakers[id][0]['topic-information']);
+
+  $('.topic-item-2 .speaker-info .speaker-img img').attr('src', speakers[id][1]['producer-img']).attr('alt', speakers[id][1]['producer-name']);
+  $('.topic-item-2 .speaker-info .speaker-detail .speaker-detail-introduction .speaker-name').append(speakers[id][1]['producer-name']);
+  $('.topic-item-2 .speaker-info .speaker-detail .speaker-detail-introduction .speaker-title').append(speakers[id][1]['producer-introduction']);
+  $('.topic-item-2 .speaker-detail .speaker-detail-information').append(speakers[id][1]['producer-information']);
+
+  $('.topic-item-2 .speaker-info .speaker-topic .speaker-topic-content').append(speakers[id][1]['producer-topic']);
+  $('.topic-item-2 .speaker-info .speaker-topic-detail .speaker-topic-content-detail').append(speakers[id][1]['topic-information']);
+});
