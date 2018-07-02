b090787385ba12ecd4de9f0398a2a0055cea8c99
Piao Luo <piluo@thoughtworks.com>
2018-07-02 15:45:35 +0800

    feature: change producers info page as a list page

--- docs/js/producer.js
+++ docs/js/producer.js
@@ -1,24 +1,45 @@
 $(function () {
-  var id = Number(window.location.search.split('=')[1]) - 1
-  console.log(id)
-  $('.speaker-info .speaker-img img').attr('src', producers[id]['producer-img']).attr('alt', producers[id]['producer-name']);
-  $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-name').append(producers[id]['producer-name']);
-  $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-title').append(producers[id]['producer-introduction']);
-  $('.speaker-detail .speaker-detail-information').append(producers[id]['producer-information']);
+  var templateHTMl = $('<div class="grid producer-grid-space centered">\n' +
+    '        <div class="grid-cell speaker-info text-left">\n' +
+    '            <div class="flexbox-row">\n' +
+    '                <div class="speaker-img speaker-intro-row">\n' +
+    '                    <div>\n' +
+    '                        <img src="" alt="">\n' +
+    '                        <div class="speaker-topic-import-icon"></div>\n' +
+    '                    </div>\n' +
+    '                </div>\n' +
+    '                <div class="speaker-detail speaker-intro-row">\n' +
+    '                    <div class="speaker-detail-introduction">\n' +
+    '                        <div class="speaker-name"></div>\n' +
+    '                        <div class="speaker-title"></div>\n' +
+    '                    </div>\n' +
+    '                    <div class="speaker-detail-information"></div>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '\n' +
+    '            <div class="flexbox-row">\n' +
+    '                <div class="speaker-topic speaker-intro-row">\n' +
+    '                    <div class="speaker-topic-title">出品主题</div>\n' +
+    '                    <div class="speaker-topic-content"></div>\n' +
+    '                </div>\n' +
+    '                <div class="speaker-topic-detail flexbox-item grid-grey">\n' +
+    '                    <div class="speaker-topic-content-detail"></div>\n' +
+    '                </div>\n' +
+    '            </div>\n' +
+    '        </div>\n' +
+    '\n' +
+    '    </div>')
 
-  $('.speaker-info .speaker-topic .speaker-topic-content').append(producers[id]['producer-topic']);
-  $('.speaker-info .speaker-topic-detail .speaker-topic-content-detail').append(producers[id]['topic-information']);
 
-  producers[id].speakers.forEach(speaker => {
-    console.log(speaker)
-    $("#producer-speaker-container").template("speaker");
-    $.tmpl("speaker", {
-      imgUrl: speaker.img,
-      name: speaker.name,
-      introduction: speaker.introduction,
-      information: speaker.information,
-      topic: speaker.topic,
-      topicInformation: speaker["topic-information"]
-    }).appendTo("#producers-speakers");
-  });
+  producers.forEach(function(producer) {
+    var producerWrapper = templateHTMl.clone();
+    $('.speaker-info .speaker-img img', producerWrapper).attr('src', producer['producer-img']).attr('alt', producer['producer-name']);
+    $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-name', producerWrapper).append(producer['producer-name']);
+    $('.speaker-info .speaker-detail .speaker-detail-introduction .speaker-title', producerWrapper).append(producer['producer-introduction']);
+    $('.speaker-detail .speaker-detail-information', producerWrapper).append(producer['producer-information']);
+
+    $('.speaker-info .speaker-topic .speaker-topic-content', producerWrapper).append(producer['producer-topic']);
+    $('.speaker-info .speaker-topic-detail .speaker-topic-content-detail', producerWrapper).append(producer['topic-information']);
+    $('#producer-list').append(producerWrapper)
+  })
 });
