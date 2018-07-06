07549d191176049e5c07abe3c9aa2ee6f679a677
Piao Luo <piluo@thoughtworks.com>
2018-07-06 15:29:44 +0800

    chore: replace navigation header with template and change focus function in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,68 @@
-$(function () {
-    // $('.navbar .menu-item>a').on('click', function () {
-    //     $('.navbar .menu-item').removeClass('menu-item-selected');
-    //     $(this).parent().addClass('menu-item-selected');
-    // })
-    highlightCurrentNavItem();
 
-    $(window).scroll(function () {
-        highlightCurrentNavItem();
-    });
+var NAV_ITEMS = {
+    CONFERENCE: {
+      itemIndex: 1,
+      title: 'CONFERENCE'
+    },
+    SPEAKER_INFO: {
+      itemIndex: 2,
+      title: 'SPEAKER_INFO'
+    },
+    WORKSHOP: {
+      itemIndex: 3,
+      title: 'WORKSHOP'
+    },
+    TOPIC_GATHERING: {
+      itemIndex: 4,
+      title: 'WORKSHOP'
+    },
+    LOOK_BACK: {
+      itemIndex: 5,
+      title: 'LOOK_BACK'
+    },
+    TRANSLATION: {
+      itemIndex: 6,
+      title: 'TRANSLATION'
+    },
+    BOOKING_TICKETS: {
+      itemIndex: null,
+      title: 'BOOKING_TICKETS'
+    }
+}
 
-    changeBannerSize();
-});
+var LANGUAGE = {
+  ZH: 'ZH',
+  EN: 'EN'
+}
+var TRANSLATION_LOCATIONS = {
+  [LANGUAGE.EN]: {
+    CONFERENCE: '../en/index.html#agenda',
+    SPEAKER_INFO: '../en/topic-gathering.html',
+    WORKSHOP: '../en/topic-gathering.html',
+    TOPIC_GATHERING: '../en/topic-gathering.html',
+    LOOK_BACK: '../en/look-back.html',
+    BOOKING_TICKETS: '../en/booking-tickets.html'
+  },
+  [LANGUAGE.ZH]: {
+    CONFERENCE: '../zh/index.html#agenda',
+    SPEAKER_INFO: '../zh/topic-gathering.html',
+    WORKSHOP: '../zh/topic-gathering.html',
+    TOPIC_GATHERING: '../zh/topic-gathering.html',
+    LOOK_BACK: '../zh/look-back.html',
+    BOOKING_TICKETS: '../zh/booking-tickets.html'
+  }
+}
 
 function changeBannerSize() {
     var window_height = $(window).height();
     $('.top-banner').css('height', window_height - 43);
 }
-function highlightCurrentNavItem() {
-    var window_top = $(window).scrollTop() + 43;
-    var speakers_introduction_top = $('#speakers-introduction').offset().top;
-    var conf_introduction_top = $('#conf-introduction').offset().top;
-    var partners_top = $('#partners').offset().top;
-    var location_top = $('#location').offset().top;
-    if (0 < window_top && window_top < speakers_introduction_top) {
-        focusOn(1);
-    }
-    if (speakers_introduction_top < window_top && window_top < conf_introduction_top) {
-        focusOn(2);
-    }
-    if (conf_introduction_top < window_top && window_top < partners_top) {
-        focusOn(3);
-    }
-    if (partners_top < window_top && window_top < location_top) {
-        focusOn(4);
-    }
-    if (location_top < window_top) {
-        focusOn(5);
-    }
-}
 
-function focusOn(menuItem) {
-    $('.navbar .menu-item').removeClass('menu-item-selected');
-    $('.navbar .menu-item:nth-child(' + menuItem + ')').addClass('menu-item-selected');
+function focusOn(menuItem, language) {
+  var locations = language ? TRANSLATION_LOCATIONS[language] : TRANSLATION_LOCATIONS[LANGUAGE.EN]
+  $('.navbar .menu-item').removeClass('menu-item-selected');
+  if (menuItem.itemIndex) {
+    $('.navbar .menu-item:nth-child(' + menuItem.itemIndex + ')').addClass('menu-item-selected');
+  }
+  $('.navbar .menu-item:nth-child(' + NAV_ITEMS.TRANSLATION.itemIndex + ') a').attr('href', locations[menuItem.title])
 }
